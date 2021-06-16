190ebe1d5d8daea6a63a73339024f3aee7b0ea0d
supermalio <72795376+supermalio@users.noreply.github.com>
2021-06-16 23:47:37 +0800

    Create rico-install.sh

---
+++ rico-install.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+rpm -q curl || yum install -y curl
+rpm -q wget || yum install -y wget
+suijishu=`cat /dev/urandom | tr -dc '0-9a-z' | head -c 15`
+mkdir /tmp/$suijishu
+cd /tmp/$suijishu
+wget https://raw.githubusercontent.com/961644045/HIM/master/docker-compose.yml
+read -p "请输入你的节点ID：" dockerid
+
+#配置文件
+sed -i "s|HIM|xiaoding|" docker-compose.yml
+sed -i "s|sspankey|jsq520|" docker-compose.yml
+sed -i "s|68|$dockerid|" docker-compose.yml
+sed -i "s|5109|5222|" docker-compose.yml
+sed -i "s|https://sjiuishuhfeufuebfububncissn.tiyunzong.club|https://jsq520.tk|" docker-compose.yml
+docker-compose up -d
+docker ps
+echo $suijishu
